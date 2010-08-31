05437cde0c9906dd6c3fde005d514bb7ecf2fc4f
neonstalwart <neonstalwart@gmail.com>
2010-08-31 09:56:37 -0400

    updated mustache _Templated.js to look call _attachTemplateNodes to make the tests a little more equal

--- template/mustache/_Templated.js
+++ template/mustache/_Templated.js
@@ -1,15 +1,17 @@
 dojo.provide('template.mustache._Templated');
 
 dojo.require('template.mustache._base');
+dojo.require('dijit._Templated');
 
 // this is a really lightweight version for testing
-dojo.declare('template.mustache._Templated', null, {
+dojo.declare('template.mustache._Templated', dijit._Templated, {
 	templateString: '',
 
 	buildRendering: function () {
 		var node = dojo._toDom(dojo.trim(template.mustache(this.templateString, this)));
 
 		// ignoring attaching template nodes and widgetsInTemplate for now
+		this._attachTemplateNodes(node);
 
 		if(this.domNode){
 			dojo.place(node, this.domNode, "before");
